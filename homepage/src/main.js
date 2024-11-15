84262a7abddd9ebffb647aa8c327dc3c83fdb948
TNTSuperMan <tntsuperga@gmail.com>
2024-11-15 23:43:15 +0900

    emit index props

--- homepage/src/main.js
+++ homepage/src/main.js
@@ -1,12 +1,6 @@
 import "./assets/style.css";
 import index from "./components/index";
-import { createProxy } from "./nodes/_";
-const [proxy,destroy] = createProxy({
-    hash: location.hash
-})
-window.addEventListener("hashchange",e=>{
-    proxy.hash = location.hash
-})
-const vnode = index(proxy)
+
+const vnode = index()
 vnode.ondestroy(destroy)
 document.body.appendChild(vnode.node)
