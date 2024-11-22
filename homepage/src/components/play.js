dc9872013f348f3b30f769565747e8aaf89365eb
TNTSuperMan <tntsuperga@gmail.com>
2024-11-22 18:26:49 +0900

    サンプルプログラムを整える

--- homepage/src/components/play.js
+++ homepage/src/components/play.js
@@ -6,16 +6,26 @@ export default ()=>{
         src:
 `const e = R.createVElement;
 const t = R.createVText;
+const seg = R.seg;
+const [data,destroy] = R.createProxy({value:"Hello, World!"});
 
 function title(text){
-    return e("h2", ()=>[t(()=>text)]);
+    return e("h2", ()=>[t(()=>data.value)]);
+}
+
+function input(){
+    return (seg.input
+        .value[data.value]
+        .input(e=>data.value = e.target.value)());
 }
 
 function app(){
-    return e("div", ()=>[title("Hello, World!")]);
+    return seg.div.id.app(title(),input());
 }
 
-document.body.appendChild(app().node);`,
+const vm = app();
+vm.ondestroy(destroy);
+document.body.appendChild(vm.node);`,
         apply:()=>{},
         oncrifrm(et){
             const e = et.target
