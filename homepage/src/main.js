8bf26eb70796ee55d0bc3265fc6a52a70a57db39
TNTSuperMan <tntsuperga@gmail.com>
2024-11-22 18:08:32 +0900

    fix:存在しないdestroy関数を参照するとこを削除(+削減)

--- homepage/src/main.js
+++ homepage/src/main.js
@@ -1,6 +1,4 @@
 import "./assets/style.css";
 import index from "./components/index";
 
-const vnode = index()
-vnode.ondestroy(destroy)
-document.body.appendChild(vnode.node)
+document.body.appendChild(index().node)
