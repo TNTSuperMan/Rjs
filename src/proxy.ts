8b7de5293c5a18342b3dc08e7734e66952781274
TNTSuperMan <tntsuperga@gmail.com>
2024-12-11 15:30:11 +0900

    proxyの他proxyのreactsも消す

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,9 +1,14 @@
 import { rState, subscribeReact } from "./reactive";
 
+const ReactDestroyer: ((e:symbol)=>void)[] = [];
+
 export const createProxy = <T extends object>(target: T):[T,()=>void] => {
     //           prop           reactid target    effect
     let reacts: [string|symbol, symbol, ()=>void, ()=>void][] = [];
     let childProxies: [string|Symbol, object?, (()=>void)?][] = [];
+    const destroy_react = (e: symbol) => 
+        reacts = reacts.filter(t=>t[1] != e);
+
     const {proxy, revoke} = Proxy.revocable(target, {
         get(target, prop, receiver){
             let value:any = Reflect.get(target, prop, receiver);
@@ -13,7 +18,7 @@ export const createProxy = <T extends object>(target: T):[T,()=>void] => {
                     value = findres[1];
                 }else{
                     const child_proxy = createProxy(value);
-                    childProxies.push([prop, child_proxy[0], child_proxy[1]]);
+                    childProxies.push([prop, ...child_proxy]);
                     value = child_proxy[0];
                 }
             }
@@ -26,13 +31,15 @@ export const createProxy = <T extends object>(target: T):[T,()=>void] => {
             const setret = Reflect.set(target, prop, value, receiver);
             reacts.filter(e=>e[0] == prop)
                 .forEach(e=>{
-                    reacts = reacts.filter(t=>t[1] != e[1]);
+                    ReactDestroyer.forEach(d=>d(e[1]));
                     subscribeReact(e[1], e[2], e[3]);
                 });
             return setret;
         }
     });
+    ReactDestroyer.push(destroy_react);
     return [proxy, ()=>(
+        ReactDestroyer.splice(ReactDestroyer.findIndex(e=>e==destroy_react),1),
         childProxies.forEach(e=>e[2] ? e[2]() : 0),
         revoke())];
 }
